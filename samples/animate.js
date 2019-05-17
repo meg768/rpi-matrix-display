78b797485cddb92e5bcefab4c4b79d07118c14cf
meg768 <meg768@users.noreply.github.com>
2019-05-17 19:21:14 +0200

    -

--- samples/animate.js
+++ samples/animate.js
@@ -20,6 +20,7 @@ class Command {
 		args.option('help', {describe:'Displays this information'});
 		args.option('gif',  {describe:'Specifies name of GIF', default:'pacman'});
 		args.option('duration', {describe:'Animate for a specified time (ms)'});
+		args.option('iterations', {describe:'Number of iterations to animate'});
 
 		args.wrap(null);
 
