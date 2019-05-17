320ddae2fa9e61f6ec909cc44bdaf519cb7dfd66
meg768 <meg768@users.noreply.github.com>
2019-05-17 21:13:23 +0200

    -

--- demo.js
+++ demo.js
@@ -43,8 +43,8 @@ class App {
 		try {
 			args.usage('Usage: $0 <command> [options]')
 
-            args.option('led-cols',         {describe:'Number of columns for display'});
-            args.option('led-rows',         {describe:'Number of rows for display'});
+            args.option('led-cols',         {describe:'Number of columns for display', default:64});
+            args.option('led-rows',         {describe:'Number of rows for display', default:32});
             args.option('led-gpio-mapping', {describe:'Type of hardware used', default:'regular'});
             args.option('led-rgb-sequence', {describe:'Matrix RGB color order', default:'RGB'});
             args.option('led-scan-mode',    {describe:'Scan mode (0/1)', default:0});
