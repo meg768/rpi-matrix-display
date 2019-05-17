9453e985be4b4af4f3f20a7772886cf3c27a9f8c
meg768 <meg768@users.noreply.github.com>
2019-05-17 19:55:11 +0200

    -

---
+++ sample.js
@@ -0,0 +1,23 @@
+var Matrix = require('rpi-matrix');
+var path = require("path");
+
+Matrix.Canvas.registerFont(path.join(__dirname, '../fonts/Verdana.ttf'), { family: 'what-ever' });
+
+class Sample extends Matrix {
+
+    run() {
+        var ctx = this.canvas.getContext('2d');
+
+        ctx.font = 'bold 16px Verdana';
+        ctx.fillStyle = 'blue';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillText('X', this.width / 2, this.height / 2);
+
+        this.render();
+
+        setTimeout(() => {}, 3000);
+    }
+};
+var sample = new Sample({mode:'canvas', width:32, height:32});
+sample.run();
