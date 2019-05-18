a93765859fbccb6fbf68eed2118b39eeeca930a8
meg768 <meg768@users.noreply.github.com>
2019-05-18 15:43:19 +0200

    -

--- demo.js
+++ demo.js
@@ -42,6 +42,8 @@ class App {
 
             args.option('led-cols',                {describe:'Number of columns for display', default:process.env.LED_COLS || 32});
             args.option('led-rows',                {describe:'Number of rows for display', default:process.env.LED_ROWS || 32});
+            args.option('led-chain',               {describe:'Number of daisy-chained panels', default:process.env.LED_CHAIN || 1});
+            args.option('led-parallel',            {describe:'For A/B+ models or RPi2,3b: parallel chains.', default:process.env.LED_PARALLEL || 1});
             args.option('led-gpio-mapping',        {describe:'Type of hardware used', default:'regular'});
             args.option('led-rgb-sequence',        {describe:'Matrix RGB color order', default:'RGB'});
             args.option('led-scan-mode',           {describe:'Scan mode (0/1)', default:0});
@@ -49,8 +51,6 @@ class App {
             args.option('led-pwm-bits',            {describe:'Color depth', default:11});
             args.option('led-pwm-lsb-nanoseconds', {describe:'Tweak timing', default:130});
             args.option('led-brightness',          {describe:'Display brightness', default:100});
-            args.option('led-chain',               {describe:'Number of daisy-chained panels', default:process.env.LED_CHAIN || 1});
-            args.option('led-parallel',            {describe:'For A/B+ models or RPi2,3b: parallel chains.', default:process.env.LED_PARALLEL || 1});
             args.option('led-multiplexing',        {describe:'Multiplexing type (0-4).', default:0});
             args.option('led-show-refresh',        {describe:'Show refresh rate.', default:false});
 
