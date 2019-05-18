baaae01ff0eebc6469d01fe0411b65d0649af7fa
meg768 <meg768@users.noreply.github.com>
2019-05-18 14:45:16 +0200

    -

--- src/scroll-animation.js
+++ src/scroll-animation.js
@@ -17,6 +17,8 @@ module.exports = class ScrollAnimation extends Animation  {
         this.scrollDirection = scrollDirection;
         this.scrollImage     = scrollImage;
 
+        console.log('Matrix size', this.matrix.width. this.matrix.height);
+
     }
 
     loop() {
