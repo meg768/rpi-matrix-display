7c0a23b120d692272087e61f14dc5a7d321a0999
meg768 <meg768@users.noreply.github.com>
2019-05-18 11:46:05 +0200

    -

--- demo.js
+++ demo.js
@@ -40,11 +40,15 @@ class App {
 		try {
 			args.usage('Usage: $0 <command> [options]')
 
-            args.option('led-cols',         {describe:'Number of columns for display', default:process.env.LED_COLS || 32});
-            args.option('led-rows',         {describe:'Number of rows for display', default:process.env.LED_ROWS || 32});
-            args.option('xled-gpio-mapping', {describe:'Type of hardware used', default:'regular'});
-            args.option('xled-rgb-sequence', {describe:'Matrix RGB color order', default:'RGB'});
-            args.option('xled-scan-mode',    {describe:'Scan mode (0/1)', default:0});
+            args.option('led-cols',                {describe:'Number of columns for display', default:process.env.LED_COLS || 32});
+            args.option('led-rows',                {describe:'Number of rows for display', default:process.env.LED_ROWS || 32});
+            args.option('led-gpio-mapping',        {describe:'Type of hardware used', default:'regular'});
+            args.option('led-rgb-sequence',        {describe:'Matrix RGB color order', default:'RGB'});
+            args.option('led-scan-mode',           {describe:'Scan mode (0/1)', default:0});
+            args.option('led-inverse-colors',      {describe:'Inverse colors', default:0});
+            args.option('led-pwm-bits',            {describe:'Color depth', default:11});
+            args.option('led-pwm-lsb-nanoseconds', {describe:'Tweak timing', default:130});
+            args.option('led-brightness',          {describe:'Display brightness', default:100});
 
 			this.loadSamples();  
 
