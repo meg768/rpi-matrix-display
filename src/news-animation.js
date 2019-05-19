11199428f4a47df4572ba2be0b44aa869ba25d3e
meg768 <meg768@users.noreply.github.com>
2019-05-19 18:44:29 +0200

    -

--- src/news-animation.js
+++ src/news-animation.js
@@ -37,9 +37,9 @@ module.exports = class NewsAnimation extends TextAnimation  {
 
             var request = new Request('https://newsapi.org', {debug:debug, headers:headers});
 
-            var query = {};
-            Object.assign(query, {pageSize: 1}, {category:this.category, country:this.country});
-
+            //var query = {};
+            //Object.assign(query, {pageSize: 1}, {category:this.category, country:this.country});
+            var query = {...{pageSize: 1},  ...{category:this.category, country:this.country}};
             console.log('QUERY:', query);
 
             request.get('/v2/top-headlines', {query:query}).then((response) => {
