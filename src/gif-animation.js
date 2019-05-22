fbb5618dbf0949c8ecca7e8a8609843bbb48b29b
meg768 <meg768@users.noreply.github.com>
2019-05-22 17:44:52 +0200

    -

--- src/gif-animation.js
+++ src/gif-animation.js
@@ -72,7 +72,7 @@ module.exports = class GifAnimation extends Animation {
 
     constructor(options) {
 
-        var {name = 'pacman', iterations = undefined, ...other} = options;
+        var {name = undefined, iterations = undefined, ...other} = options;
 
         super(other);
 
