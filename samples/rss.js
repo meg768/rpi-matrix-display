c4cef6d6c02e675c79412c07c350719c86bd966d
meg768 <meg768@users.noreply.github.com>
2020-11-04 19:38:20 +0100

    -

--- samples/rss.js
+++ samples/rss.js
@@ -172,13 +172,13 @@ class Command {
             debug = console.log;
 
         var feeds = [
-            {url: 'https://digital.di.se/rss',                                        name: 'DI               '},
-            {url: 'https://www.sydsvenskan.se/rss.xml?latest',                        name: 'Sydsvenskan      '},
+            {url: 'https://digital.di.se/rss',                                        name: 'DI'},
+            {url: 'https://www.sydsvenskan.se/rss.xml?latest',                        name: 'Sydsvenskan'},
             {url: 'http://www.svd.se/?service=rss',                                   name: 'Svenska Dagbladet'},
-            {url: 'https://rss.aftonbladet.se/rss2/small/pages/sections/aftonbladet', name: 'Aftonbladet      '},
-            {url: 'https://feeds.expressen.se/nyheter',                               name: 'Expressen        '},
-            {url: 'http://feeds.bbci.co.uk/news/rss.xml',                             name: 'BBC              '},
-            {url: 'https://rss.nytimes.com/services/xml/rss/nyt/World.xml',           name: 'New York Times   '}
+            {url: 'https://rss.aftonbladet.se/rss2/small/pages/sections/aftonbladet', name: 'Aftonbladet'},
+            {url: 'https://feeds.expressen.se/nyheter',                               name: 'Expressen'},
+            {url: 'http://feeds.bbci.co.uk/news/rss.xml',                             name: 'BBC'},
+            {url: 'https://rss.nytimes.com/services/xml/rss/nyt/World.xml',           name: 'New York Times'}
         ];
 
         var news = [];
