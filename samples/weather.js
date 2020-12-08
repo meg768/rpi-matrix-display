a1aab342e1163291703ffac3dad53291d216ef91
meg768 <meg768@users.noreply.github.com>
2020-12-08 17:55:36 +0100

    -

--- samples/weather.js
+++ samples/weather.js
@@ -32,10 +32,10 @@ class OpenWeatherMap {
             query.lang    = 'se';
     
             request.get('/data/2.5/onecall', {query:query}).then((response) => {
-                response.body.current.dt = new Date(response.body.current.dt).toLocaleString();
+                response.body.current.dt = new Date(response.body.current.dt).toString();
 
                 response.body.daily.forEach(element => {
-                    element.dt = new Date(element.dt).toLocaleString();
+                    element.dt = new Date(element.dt).toString();
                 });
                 resolve(response);
             })
