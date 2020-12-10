d3530ba5c69a155ed5a608aa7f2e27a16c61b2a3
meg768 <meg768@users.noreply.github.com>
2020-12-10 17:24:27 +0100

    -

--- samples/weather.js
+++ samples/weather.js
@@ -18,26 +18,40 @@ class OpenWeatherMap {
     parseWeather(weather) {
 
     }
+    
     fetch() {
         return new Promise((resolve, reject) => {
             var Request = require('yow/request');
             var request = new Request('http://api.openweathermap.org');
     
             var query = {};
-            query.lat     = process.env.OPENWEATHERMAP_LAT;
-            query.lon     = process.env.OPENWEATHERMAP_LON;
             query.appid   = process.env.OPENWEATHERMAP_APPID;
+            query.lat     = 55.71;
+            query.lon     = 13.19;
             query.exclude = 'minutely,hourly';
             query.units   = 'metric';
             query.lang    = 'se';
     
             request.get('/data/2.5/onecall', {query:query}).then((response) => {
-                response.body.current.dt = new Date(response.body.current.dt * 1000).toString();
+                response.body.current.dt = new Date(response.body.current.dt * 1000);
+                response.body.current.sunset = new Date(response.body.current.sunset * 1000);
+                response.body.current.sunrise = new Date(response.body.current.sunrise * 1000);
+
+                var current  = response.body.current;
+                var tomorrow = response.body.daily[1];
+
+                var texts = [];
+                texts.push(sprintf('Idag %0f° och %s', current.temp, current.weather.description));
+                texts.push(sprintf('I morgon %0f° (%0f°) och %s', tomorrow.weather.max, tomorrow.weather.min, tomorrow.weather.description));
 
                 response.body.daily.forEach(element => {
-                    element.dt = new Date(element.dt * 1000).toString();
+                    element.dt = new Date(element.dt * 1000);
+                    element.sunset = new Date(element.sunset * 1000);
+                    element.sunrise = new Date(element.sunrise * 1000);
                 });
-                resolve(response);
+
+                console.log(response.body);
+                resolve(texts);
             })
             .catch((error) => {
                 reject(error);
