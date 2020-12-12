5f701ebff3f7550f9d036afa0420f597739b059d
meg768 <meg768@users.noreply.github.com>
2020-12-12 18:14:49 +0100

    -

--- src/command.js
+++ src/command.js
@@ -5,45 +5,39 @@ module.exports = class Command {
     constructor(module, options) {
         var {command, description} = options;
 
+        var builder = (argv) => {
+            this.argv = argv;
+            this.run(argv);
+
+        };
+
+        var handler = (args) => {
+            args.usage('Usage: $0 [options]');
+
+            args.option('help', {describe:'Displays this information'});
+            args.option('debug', {describe:'Debug mode', default:undefined});
+    
+            this.defineOptions(args);
+    
+            args.wrap(null);
+    
+            args.check(function(argv) {
+                return true;
+            });
+    
+            return args.argv;
+
+        };
+
         module.exports.command  = command;
         module.exports.describe = description;
-        module.exports.builder  = this.builder.bind(this);
-        module.exports.handler  = this.handler.bind(this);
+        module.exports.builder  = builder.bind(this);
+        module.exports.handler  = handler.bind(this);
 
         this.debug = console.log;
 
     }
 
-    defineOptions(args) {
-        console.log('DEFINING OPTIONS command');
-    }
- 
-    handler(args) {
-
-        args.usage('Usage: $0 [options]');
-
-        args.option('help', {describe:'Displays this information'});
-        args.option('debug', {describe:'Debug mode', default:undefined});
-
-        this.defineOptions(args);
-
-        args.wrap(null);
-
-        args.check(function(argv) {
-            return true;
-        });
-
-        return args.argv;
-    }
-    
-    
-    builder(argv) {
-        this.argv = argv;
-        this.run(argv);
-    }
-
-
-    
 
 
 };
