3fecc9aef272c581796fcc24bb2bf587a3f36a6c
meg768 <meg768@users.noreply.github.com>
2020-12-15 18:08:40 +0100

    -

--- samples/weather.js
+++ samples/weather.js
@@ -38,7 +38,7 @@ module.exports = class WeatherCommand extends MatrixCommand {
         this.enqueueAnimations();
 
         this.queue.on('idle', () => {
-            this.timer.SetTimer(60 * 1000, () => {
+            this.timer.setTimer(60 * 1000, () => {
                 this.enqueueAnimations();
             })
         });
