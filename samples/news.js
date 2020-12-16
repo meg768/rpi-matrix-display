4e469a0f055b0c713c3454f90e2a99fcc38c5774
meg768 <meg768@users.noreply.github.com>
2020-12-16 18:15:26 +0100

    -

--- samples/news.js
+++ samples/news.js
@@ -1,8 +1,5 @@
 
-var TextAnimation = require('../src/text-animation.js');
-var WeatherAnimation = require('../src/weather-animation.js');
 var NewsAnimation = require('../src/news-animation.js');
-var NewsFeed = require('../src/news-feed.js');
 var MatrixCommand = require('../src/matrix-command.js');
 
 module.exports = class extends MatrixCommand {
@@ -22,27 +19,6 @@ module.exports = class extends MatrixCommand {
 
     enqueueAnimations() {
         this.queue.enqueue(new NewsAnimation({...this.argv}));
-    }
-
-    XXXenqueueAnimations() {
-        var sprintf = require('yow/sprintf');
-
-        return new Promise((resolve, reject) => {
-
-            var feed = new NewsFeed(this.argv);
-    
-            feed.fetch().then((news) => {
-                news.forEach((item) => {
-                    var text = sprintf('%s - %s', item.description, item.title);
-                    this.queue.enqueue(new TextAnimation({...this.argv, text:text}));
-                });
-
-                resolve();
-            })
-            .catch((error) => {
-                reject(error);
-            });
-        });
     }
 
 
