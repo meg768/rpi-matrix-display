0eda515f3baecabf8ed916178781a85d592ae579
meg768 <meg768@users.noreply.github.com>
2021-01-03 17:46:21 +0100

    -

--- samples/server.js
+++ samples/server.js
@@ -20,26 +20,13 @@ module.exports = class extends MatrixCommand {
     }
 
     setupExpress() {
-        var express = require('express');
         var bodyParser = require('body-parser');
-        var app = express();
+		var app = require('express')();
+		var server = require('http').createServer(app);
+		var io = require('socket.io')(server);
 
         app.use(bodyParser.urlencoded({ limit: '50mb', extended: false}));
         app.use(bodyParser.json({limit: '50mb'}));
-/*
-
-		for (var animationName in this.animations) {
-			app.post(`/${animationName}`, (request, response) => {
-				try {
-					this.runAnimation(animationName, {...request.query, ...request.body});
-					response.status(200).json({status:'OK'});    
-				}
-				catch(error) {
-					response.status(401).json({status:error.message});    
-				}
-			});           			
-		}
-*/
 
 		app.post(`/animate/:animation`, (request, response) => {
 			try {
@@ -51,9 +38,9 @@ module.exports = class extends MatrixCommand {
 			}
 		});           
 
-		app.post(`/news`, (request, response) => {
+		app.get(`/animate/:animation`, (request, response) => {
 			try {
-				this.runAnimation('news', {...request.query, ...request.body});
+				this.runAnimation(request.params.animation, {...request.query, ...request.body});
 				response.status(200).json({status:'OK'});    
 			}
 			catch(error) {
@@ -61,58 +48,41 @@ module.exports = class extends MatrixCommand {
 			}
 		});           
 
-		app.get(`/text`, (request, response) => {
-			try {
-				this.runAnimation('text', {...request.body, ...request.query});
-				response.status(200).json({status:'OK'});    
-			}
-			catch(error) {
-				response.status(401).json({status:error.message});    
-			}
-		});           
+		io.on('connection', (socket) => {
+			console.log('A user connected');
 
-		app.post(`/text`, (request, response) => {
-			try {
-				this.runAnimation('text', {...request.query, ...request.body});
-				response.status(200).json({status:'OK'});    
-			}
-			catch(error) {
-				response.status(401).json({status:error.message});    
-			}
-		});           
+			socket.on('disconnect', () => {
+				console.log('A user disconnected');
+			});	
 
-		app.post(`/gif`, (request, response) => {
-			try {
-				this.runAnimation('gif', {...request.query, ...request.body});
-				response.status(200).json({status:'OK'});    
-			}
-			catch(error) {
-				response.status(401).json({status:error.message});    
-			}
-		});           
+			socket.on('animate', (animation, payload, fn) => {
 
-		app.post(`/rain`, (request, response) => {
-			try {
-				this.runAnimation('rain', {...request.query, ...request.body});
-				response.status(200).json({status:'OK'});    
-			}
-			catch(error) {
-				response.status(401).json({status:error.message});    
-			}
-		});           
+				var callback = (args) => {
+					if (typeof fn == 'function') {
+						console.log(`Callback is a function - calling...`);
+						fn(args);
 
-		app.post(`/weather`, (request, response) => {
-			try {
-				this.runAnimation('weather', {...request.query, ...request.body});
-				response.status(200).json({status:'OK'});    
-			}
-			catch(error) {
-				response.status(401).json({status:error.message});    
-			}
-		});           
+					}
+					else {
+						console.log('Callback is not a function', callback);
+					}
+				};				
+
+				try {
+					this.runAnimation(animation, payload);
+					callback({status:'OK'});
+				}
+				catch(error) {
+					console.error(error.message);
+					callback({status:error.message});
+				}
+	
+			});
+
+		});
 
 
-        app.listen(this.argv.port);
+        server.listen(this.argv.port);
 
 	}
 	
@@ -120,7 +90,9 @@ module.exports = class extends MatrixCommand {
 		var Animation = this.animations[name];
 		
 		if (Animation == undefined)
-			throw new Error(`Animation '${name}' was not found`);
+			throw new Error(`Animation '${name}' was not found.`);
+
+		console.log(`Displaying animation ${name} with payload ${options}...`);
 
 		this.queue.enqueue(new Animation(options));
 	}
