9907263c850b05e4b966ec3fdb05695fa88a1972
meg768 <meg768@users.noreply.github.com>
2021-03-15 21:48:05 +0100

    -

--- src/text-animation.js
+++ src/text-animation.js
@@ -21,11 +21,10 @@ var loadEmojis = once((folder) => {
 
 			if (parts.length == 2) {
 				var name = parts[0].toLowerCase();
-				var code = parts[1].toUpperCase();
+				var code = sprintf('%04X', parseInt(parts[1], 16));
 
 				images[name] = fileName;
 				images[code] = fileName;
-				images['0'+ code] = fileName;
 
 				console.log(`${name}=${fileName}`)
 				console.log(`${code}=${fileName}`)
