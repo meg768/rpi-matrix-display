17debaa63549d6241011891a4d00374612ea9801
meg768 <meg768@users.noreply.github.com>
2021-03-16 18:01:31 +0100

    -

--- samples/mqtt.js
+++ samples/mqtt.js
@@ -12,6 +12,7 @@ module.exports = class extends MatrixCommand {
 		this.hostname = OS.hostname();
 		this.timer = new Timer();
 		this.texts = [];
+		this.config = {};
 
 	}
 
@@ -38,28 +39,71 @@ module.exports = class extends MatrixCommand {
 		this.queue.enqueue(new TextAnimation({...this.argv, iterations:1, text:`${text}`}));
 	}
 
+	runAnimation(name, options) {
+
+		var TextAnimation    = require('../src/text-animation.js');
+		var RainAnimation    = require('../src/rain-animation.js');
+		var NewsAnimation    = require('../src/news-animation.js');
+		var WeatherAnimation = require('../src/weather-animation.js');
+		var GifAnimation     = require('../src/gif-animation.js');
+
+		var animations = {};
+		animations['text']    = TextAnimation;
+		animations['rain']    = RainAnimation;
+		animations['weather'] = WeatherAnimation;
+		animations['gif']     = GifAnimation;
+		animations['news']    = NewsAnimation;		
+		
+		var Animation = this.animations[name];
+		var config = this.config[name] || {};
+
+		options = {...config, ...options};
+		
+		if (Animation == undefined)
+			throw new Error(`Animation '${name}' was not found.`);
+
+		this.debug(`Displaying animation '${name}' with payload ${JSON.stringify(options)}...`);
+		this.queue.enqueue(new Animation(options));
+	}
+	
 	async start() {
 		await super.start();
+
+		this.config = {};
 
 		this.debug(`Connecting to host '${this.argv.host}'...`);
 		var mqtt = MQTT.connect(this.argv.host, {username:process.env.MQTT_USERNAME, password:process.env.MQTT_PASSWORD, port:process.env.MQTT_PORT});
 
 		mqtt.on('connect', () => {
-			this.displayText(`Connected to ${this.argv.host}:${this.argv.port}... 🙂`);
+			this.log(`Connected to ${this.argv.host}:${this.argv.port}...`);
 		})
 
 		//mqtt.subscribe('RSS/#');
-		mqtt.subscribe(`Raspberry/${this.hostname}/text`);
+		mqtt.subscribe(`Raspberry/${this.hostname}/#`);
 
-		mqtt.on(`Raspberry/${this.hostname}/text`, (topic, message, args) => {
+		mqtt.on(`Raspberry/${this.hostname}/:animation`, (topic, message, args) => {
 
 			try {
-				this.queue.enqueue(new TextAnimation({...this.argv, priority:'!', iterations:1, text:`${message}`}));
+				var payload = JSON.parse(message);
+				this.debug(`Config ${args.animation}:${JSON.stringify(payload)}`);
+				this.config[args.animation] = JSON.parse(message);
 			}
 			catch(error) {
 				this.log(error);
 			}
 		});
+
+		mqtt.on(`Raspberry/${this.hostname}/:animation/animate`, (topic, message, args) => {
+
+			try {
+				var payload = JSON.parse(message);
+				this.runAnimation(args.animation, payload);
+			}
+			catch(error) {
+				this.log(error);
+			}
+		});
+
 
 
 		mqtt.on('RSS/:name/title', (topic, message, args) => {
