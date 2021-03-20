18a03ae1a7749c9feb59d25f588018558f2daee2
meg768 <meg768@users.noreply.github.com>
2021-03-20 19:21:22 +0100

    -

--- samples/mqtt.js
+++ samples/mqtt.js
@@ -70,7 +70,7 @@ module.exports = class extends MatrixCommand {
 			this.runAnimation('text', {text:'🤪'});
 		})
 
-		mqtt.subscribe('RSS/+/+');
+		//mqtt.subscribe('RSS/+/+');
 		mqtt.subscribe('Yahoo Quotes/+/+');
 		
 		/*
@@ -137,7 +137,7 @@ module.exports = class extends MatrixCommand {
 					if (quote.price != undefined && quote.change != undefined) {
 
 						//let text = sprintf('%s - %.02f (%.01f%%)', args.name, quote.price, quote.change);
-						let text = sprintf('%s %s %.01f%%', args.name, quote.change < 0 ? '-' : '+', quote.change);
+						let text = sprintf('%s %s%.01f%%', args.name, quote.change < 0 ? '-' : '+', quote.change);
 						this.runAnimation('text', {...this.argv, iterations:1, text:text, textColor:quote.change < 0 ? 'red' : 'blue'});
 	
 					} 
