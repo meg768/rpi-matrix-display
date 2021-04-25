469746ef0a5948002fffbca619a7d882c0c6eb85
meg768 <meg768@users.noreply.github.com>
2021-04-25 17:47:25 +0200

    -

--- src/news-animation.js
+++ src/news-animation.js
@@ -21,17 +21,35 @@ module.exports = class extends TextAnimation {
             {url:'https://rss.aftonbladet.se/rss2/small/pages/sections/aftonbladet', name: 'Aftonbladet',    description:'Aftonbladet'}
         ];
 
+		
 
     }
 
 
-    fetch(feed) {
+    async fetch(feed) {
         var sprintf = require('yow/sprintf');
 
+		this.debug(`Fetching ${feed.url}...`);
+		var result = await this.parser.parseURL(feed.url);
+		this.debug(`Finished fetching from ${feed.url}...`);
+
+		result.items.forEach((item) => {
+			var key = sprintf('%s:%s', item.isoDate, item.title);
+			var news = {};
+
+			news.timestamp   = new Date(item.isoDate);
+			news.name        = feed.name;
+			news.description = feed.description;
+			news.title       = item.title;
+
+			this.cache[key]  = news;
+		});
+
+
+/*
         return new Promise((resolve, reject) => {
 
-			this.debug(`Fetching ${feed.url}...`);
-			
+
             this.parser.parseURL(feed.url).then((result) => {
 
                 result.items.forEach((item) => {
@@ -52,15 +70,68 @@ module.exports = class extends TextAnimation {
 
             })
         });
+		*/
     }
 
-    getText() {
+	async getText() {
+		Promise.all(this.feeds.map(async (feed) => {
+			await this.fetch(feed);
+		})); 
+
+		var now = new Date();
+		var someTimeAgo = new Date(now.getTime() -  1 * 24 * 60 * 60 * 1000);
+		var news = [];        
+		var cache = {};
+
+		// Remove old entries
+		for (var key in this.cache) {
+			var item = this.cache[key];
+
+			if (item.timestamp.getTime() >= someTimeAgo.getTime())
+				cache[key] = item;
+		}
+
+		for (var key in cache) {
+			news.push(cache[key]);
+		}
+
+		// Sort by date DESC
+		news.sort((a, b) => {
+			return b.timestamp.getTime() - a.timestamp.getTime();
+		});
+
+		// Select top 5
+		news = news.slice(0, 5);
+
+		// Save cache for later
+		this.cache = cache;
+
+		var text = [];
+
+		news.forEach((item) => {
+			text.push(`${item.description} - ${item.title}`);
+		});
+
+		return text;
+	}
+
+
+    getTextOld() {
 
         return new Promise((resolve, reject) => {
 
             var promises = [];
             
             this.feeds.forEach((feed) => {
+
+				
+				try {
+					var promise = this.fetch(feed);
+
+				}
+				catch(error) {
+
+				}
                 promises.push(this.fetch(feed));
             });
 
