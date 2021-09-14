d208afd93148e678f1035a271eb53b175e6a6d39
meg768 <meg768@users.noreply.github.com>
2021-09-14 17:35:41 +0200

    -

--- samples/loop.js
+++ samples/loop.js
@@ -56,7 +56,7 @@ module.exports = class extends MatrixCommand {
         if (this.argv.gif) {
             this.animations.push(() => {
                 var Animation = require('../src/gif-animation.js');
-                return new Animation({...this.argv, duration:30000});                
+                return new Animation({...this.argv, duration:5 * 60 * 1000});                
             });
         }
 
