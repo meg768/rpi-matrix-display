804bc11171b29a5fe9ca44e669fb491dd0602129
meg768 <meg768@users.noreply.github.com>
2022-06-15 21:56:55 +0200

    -

--- src/rss-animation.js
+++ src/rss-animation.js
@@ -1,43 +1,38 @@
 var TextAnimation = require('./text-animation.js');
-let RSS = require('./rss-parser-events');
-let rss = null;
+let headlines = [];
+
+function startUp() {
+    let RSS = require('./rss-parser-events');
+
+    let feeds = {
+        "BBC": "http://feeds.bbci.co.uk/news/uk/rss.xml#",
+        "CNN": "http://rss.cnn.com/rss/edition.rss",
+        "Google": "https://news.google.com/rss?gl=US&ceid=US:en&hl=en-US"
+    };
+
+    let options = {
+        interval:1
+    }
+
+    let rss = new RSS(feeds, options);
+
+    rss.on('rss', (name, rss) => {
+        let text = `${name} - ${rss.title}`;
+        console.log(text);
+        headlines.push(text);
+    });
+
+}
+
 
 module.exports = class extends TextAnimation {
 
     constructor(options = {}) {
         super(options);
 
-        this.text = [];
-
     }
 
     async start() {
-
-        if (rss == null) {
-
-            let feeds = {
-                "BBC": "http://feeds.bbci.co.uk/news/uk/rss.xml#",
-                "CNN": "http://rss.cnn.com/rss/edition.rss",
-                "Google": "https://news.google.com/rss?gl=US&ceid=US:en&hl=en-US"
-            };
-
-            let options = {
-                interval:1
-            }
-
-            rss = new RSS(feeds, options);
-
-            rss.on('rss', (name, rss) => {
-                let text = `${name} - ${rss.title}`;
-                console.log(text);
-                this.text = ['AB', 'CD'];
-
-
-            });
-
-    
-        }
-
 
         await super.start();
 
@@ -50,9 +45,7 @@ module.exports = class extends TextAnimation {
 
 
 	async getText() {
-//        return (['A', 'B']);
-        console.log('GET TEXT', this.text);
-        return this.text;
+        return headlines;
 
 	}
 
