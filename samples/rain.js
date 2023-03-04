b58f7f8afd4480f45a339c066175e60bde6b896c
meg768 <meg768@users.noreply.github.com>
2023-03-04 21:45:24 +0100

    -

--- samples/rain.js
+++ samples/rain.js
@@ -16,7 +16,7 @@ module.exports = class RainCommand extends MatrixCommand {
 	}
 
 	async run() {
-		var animation = new RainAnimation({...this.argv, debug:this.debug});		
+		var animation = new RainAnimation({...this.argv);		
 		return animation.run();
 	}
 
