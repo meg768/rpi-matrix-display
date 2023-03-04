67edb32f62e441ca212a2db4ec56fd1029240c3a
meg768 <meg768@users.noreply.github.com>
2023-03-04 21:43:21 +0100

    -

--- app.js
+++ app.js
@@ -36,45 +36,9 @@ class App {
 		})
 
 	}
-/*
-	getDefaultConfig() {
-		var config = {};
 
-		function getValue(value, defaultValue) {
-			if (value == undefined)
-				return defaultValue;
-
-			if (typeof defaultValue == 'number')
-				return parseInt(value);
-
-			if (typeof defaultValue == 'boolean')
-				return parseInt(value) != 0;
-			
-			return value;
-		}
-
-		config['led-gpio-mapping'] = getValue(process.env.LED_GPIO_MAPPING, 'regular');
-		config['led-rows'] = getValue(process.env.LED_ROWS, 32);
-		config['led-cols'] = getValue(process.env.LED_COLS, 32);
-		config['led-chain'] = getValue(process.env.LED_CHAIN, 1);
-		config['led-parallel'] = getValue(process.env.LED_PARALLEL, 1);
-		config['led-multiplexing'] = getValue(process.env.LED_MULTIPLEXING, 0);
-		config['led-pwm-bits'] = getValue(process.env.LED_PWM_BITS, 11);
-		config['led-brightness'] = getValue(process.env.LED_BRIGHTNESS, 100);
-		config['led-scan-mode'] = getValue(process.env.LED_SCAN_MODE, 0);
-		config['led-row-addr-type'] = getValue(process.env.LED_ROW_ADDR_TYPE, 0);
-		config['led-show-refresh'] = getValue(process.env.LED_SHOW_REFRESH, false);
-		config['led-inverse'] = getValue(process.env.LED_INVERSE, false);
-		config['led-rgb-sequence'] = getValue(process.env.LED_RGB_SEQUENCE, 'RGB');
-		config['led-pwm-lsb-nanoseconds'] = getValue(process.env.LED_PWM_LSB_NANOSECONDS, 130);
-		config['led-pwm-dither-bits'] = getValue(process.env.LED_PWM_DITHER_BITS, 0);
-		config['led-no-hardware-pulse'] = getValue(process.env.LED_NO_HARDWARE_PULSE, false);
-		config['led-slowdown-gpio'] = getValue(process.env.LED_SLOWDOWN_GPIO, 1);
-		config['led-pwm-dither-bits'] = getValue(process.env.LED_PWM_DITHER_BITS, 0);
-
-		return config;
-	}
-*/
+    
+    
 	run() {
 
 		try {
