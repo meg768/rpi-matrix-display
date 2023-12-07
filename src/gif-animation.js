c9d946967b0de9ec1b7793f8f2dabc7117749ed3
meg768 <meg768@users.noreply.github.com>
2023-12-07 20:01:33 +0100

    -

--- src/gif-animation.js
+++ src/gif-animation.js
@@ -114,8 +114,8 @@ module.exports = class GifAnimation extends Animation {
             ctx.imageSmoothingEnabled = false;
 
         if (this.iterations != undefined) {
-            console.log(`Iterations: ${this.iterations}`);
             this.iterations = this.iterations * this.gif.frameCount;
+            console.log(`Iterations: ${this.iterations}`);
         }
 
         this.gif = gif;
